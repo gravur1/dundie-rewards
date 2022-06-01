4eb319ec301e27148cc84c0ff44ea6cb08a171bd
gravur1 <tiagometur@gmail.com>
2022-06-01 17:52:22 -0300

    Turned package installable (#12)

--- setup.py
+++ setup.py
@@ -6,4 +6,9 @@ setup(
     description="Reward point system for Dunder Mifflin",
     author="Tiago Turquette",
     packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "dundie = dundie.__main__:main"
+        ]
+    }
 )
