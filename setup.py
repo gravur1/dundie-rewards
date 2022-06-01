5be6d059aead3af2a31fd403ff411881ea0c1670
Tiago Turquette <tiagometur@gmail.com>
2022-05-31 23:20:34 -0300

    Setup project initial organization and metadata
    - Add setup.py to turn package installable
    - Add __main__ for executable
    - Add __init__ for package
    
    closes #8

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "dundie",
+    version = "0.1.0",
+    description="Reward point system for Dunder Mifflin",
+    author="Tiago Turquette",
+    packages=find_packages(),
+)
